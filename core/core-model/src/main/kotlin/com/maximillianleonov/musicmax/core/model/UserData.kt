4f91a2efec377019ed9548a470e6581f09639b85
Maximillian Leonov <maximillianleonov@gmail.com>
2023-04-12 10:13:32 +0400

    Add favoriteSongs to UserData model

--- core/core-model/src/main/kotlin/com/maximillianleonov/musicmax/core/model/UserData.kt
+++ core/core-model/src/main/kotlin/com/maximillianleonov/musicmax/core/model/UserData.kt
@@ -18,5 +18,6 @@ package com.maximillianleonov.musicmax.core.model
 
 data class UserData(
     val playingQueueIds: List<String>,
-    val playingQueueIndex: Int
+    val playingQueueIndex: Int,
+    val favoriteSongs: Set<String>
 )
