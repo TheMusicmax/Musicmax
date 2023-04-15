5185b9c83b8e36bf4115400a336086db202a18e8
Maximillian Leonov <maximillianleonov@gmail.com>
2023-04-15 15:12:49 +0400

    Change musicmax page url

--- core/core-data/src/main/kotlin/com/maximillianleonov/musicmax/core/data/util/Constants.kt
+++ core/core-data/src/main/kotlin/com/maximillianleonov/musicmax/core/data/util/Constants.kt
@@ -18,7 +18,7 @@ package com.maximillianleonov.musicmax.core.data.util
 
 internal object Constants {
     internal object Urls {
-        private const val MUSICMAX_PAGE_URL = "https://maximillianleonov.github.io/Musicmax"
+        private const val MUSICMAX_PAGE_URL = "https://themusicmax.github.io/"
         internal const val MUSICMAX_REPO_URL = "https://github.com/MaximillianLeonov/Musicmax"
         internal const val PRIVACY_POLICY_URL = "$MUSICMAX_PAGE_URL/privacy-policy.html"
     }
