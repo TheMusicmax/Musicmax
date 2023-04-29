f73e4851cd7c3495ef807858f309e71f90e23dc7
Maximillian Leonov <maximillianleonov@gmail.com>
2023-04-29 09:57:54 +0400

    Add sorting options to UserData model

--- core/core-model/src/main/kotlin/com/maximillianleonov/musicmax/core/model/UserData.kt
+++ core/core-model/src/main/kotlin/com/maximillianleonov/musicmax/core/model/UserData.kt
@@ -20,7 +20,11 @@ data class UserData(
     val playingQueueIds: List<String>,
     val playingQueueIndex: Int,
     val playbackMode: PlaybackMode,
+    val sortOrder: SortOrder,
+    val sortBy: SortBy,
+
     val favoriteSongs: Set<String>,
+
     val darkThemeConfig: DarkThemeConfig,
     val useDynamicColor: Boolean
 )
